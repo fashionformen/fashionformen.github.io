900ee8225c1cc514fdd342b9abc8ca721aa843ed
username <90975164+adamswebsite@users.noreply.github.com>
2024-01-15 09:33:18 +0100

    yes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,10 @@
-import image from "@astrojs/image";
-import mdx from "@astrojs/mdx";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-import config from "./src/config/config.json";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: config.site.base_url ? config.site.base_url : "https://fashionformen.github.io/",
-  base: config.site.base_path ? config.site.base_path : "/",
-  trailingSlash: config.site.trailing_slash ? "always" : "never",
-  integrations: [
-    react(),
-    sitemap(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    mdx(),
-  ],
-  markdown: {
-    remarkPlugins: [],
-    shikiConfig: {
-      theme: "one-dark-pro",
-      wrap: true,
-    },
-    extendDefaultPlugins: true,
-  },
+	site: 'https://fashionformen.github.io/',
+	integrations: [mdx(), sitemap()],
 });
